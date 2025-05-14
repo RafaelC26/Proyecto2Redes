e87f3d3c73f998cb1d4191d642d01951987d9192
Rafa <rafael.ctristancho01@uptc.edu.co>
2025-05-14 11:48:32 -0500

    clase principal

--- ProyectoRedesSimulacion/src/UI/Principal.java
+++ ProyectoRedesSimulacion/src/UI/Principal.java
@@ -1,7 +1,26 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Principal extends JFrame {
     public void start(){
+        setTitle("Simulador de Redes de Computadores");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setLayout(null);
 
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setLayout(null);
+        panelPrincipal.setBounds(0, 0, this.WIDTH, this.HEIGHT);
+        
+
+
+        add(panelPrincipal);
+
+
+
+
+        setVisible(true);
     }
 }
